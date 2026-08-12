c7215e0d7bc5021a34af234c56b3936debde5b1c
Gusthi Pangestu <gusthipangestu1906@gmail.com>
2026-08-12 21:37:22 +0700

    Fix Vite base path for Firebase Hosting

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Tambahkan baris ini. GANTI 'ukm-report-app' DENGAN NAMA REPOSITORY-MU
-  base: '/ukm-report-app/', 
+  base: '/', 
 })
